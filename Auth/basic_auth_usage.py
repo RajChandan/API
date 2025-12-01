e30f9e0126dfe6e4913275f564b0e621ad05e2c9
RajChandan <raj.chandan91@gmail.com>
2025-12-02 01:35:57 +0530

    bug fix 2

--- Auth/basic_auth_usage.py
+++ Auth/basic_auth_usage.py
@@ -1,10 +1,16 @@
-from fastapi import FastAPI, Depends
-from basic_auth import BasicAuthMiddleware, BasicAuth, InMemoryUserStore, hash_password
+from fastapi import FastAPI, Request, Depends
+from basic_auth_api import (
+    BasicAuthMiddleware,
+    BasicAuth,
+    InMemoryUserStore,
+    hash_password,
+)
 
 app = FastAPI()
 
 USERS = {"admin": hash_password("secret"), "user": hash_password("password")}
 print(USERS, " === Users")
+
 
 auth = BasicAuth(
     realm="My API", user_store=InMemoryUserStore(USERS), require_https=False
@@ -17,13 +23,15 @@ def health():
 
 
 @app.get("/admin")
-def secure(user=Depends(auth)):
-    return {"message": f"Hello, {user.username}!"}
+def secure(request: Request):
+    user = request.state.user
+    print(user, " === Authenticated User")
+    return {"message": f"Hello, {user['username']}!"}
 
 
 app.add_middleware(
     BasicAuthMiddleware,
     authenticator=auth,
     protected_prefix="/admin",
-    exempt_path=["/health", "/docs", "/redoc"],
+    exempt_path={"/health", "/docs", "/redoc"},
 )
